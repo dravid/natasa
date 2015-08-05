08fd587a25052658fa8422ff81bedda5b594f50d
dravid <vidakovic.dragan981@gmail.com>
2015-08-06 00:55:54 +0200

    naslov sajta

--- config/setup.php
+++ config/setup.php
@@ -20,7 +20,7 @@ $debug = data_setting_value($dbc, 'debug-status');
 
 $path = get_path();
 
-$site_title = 'Nataša';
+$site_title = 'Nataša - lifestyle';
 
 if(!isset($path['call_parts'][0]) || $path['call_parts'][0] == '' ){
 	//$path['call_parts'][0] = 'home';				//$pageid = $_GET['page']; // Set $pageid to equal the value given in the URL
