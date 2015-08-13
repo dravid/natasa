c42ef1b3d891e653b16ce3cb6026d33344aa193e
Dragan Vidakovic <vidakovic.dragan981@gmail.com>
2015-08-13 19:10:36 +0200

    o meni fotke saradnika

--- views/o-meni.php
+++ views/o-meni.php
@@ -40,29 +40,37 @@
 				</h2>
 				<hr>
 			</div>
-			<div class="col-sm-4 text-center">
-				<img class="img-responsive" src="http://placehold.it/375x225" alt="">
-				<h3>Saradnik1
-					<small>naziv posla</small>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 text-center saradnici">
+				<img class="img-responsive" src="img/saradnici/istvan.jpg" alt="">
+				<h3>Istvan Czekus<br>
+					<small>Photography and fashion advisor</small>
 				</h3>
 				
-				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#saradnik1">Pročitaj više</a></div>
+				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#istvan">Pročitaj više</a></div>
 			</div>
-			<div class="col-sm-4 text-center">
-				<img class="img-responsive" src="http://placehold.it/750x450" alt="">
-				<h3>Saradnik2
-					<small>naziv posla</small>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 text-center saradnici">
+				<img class="img-responsive" src="img/saradnici/goca.jpg" alt="">
+				<h3>Goca Miladinović<br>
+					<small>hair, makeup and fashion advisor</small>
 				</h3>
 				
-				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#saradnik2">Pročitaj više</a></div>
+				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#goca">Pročitaj više</a></div>
 			</div>
-			<div class="col-sm-4 text-center">
-				<img class="img-responsive" src="http://placehold.it/750x450" alt="">
-				<h3>Saradnik3
-					<small>naziv posla</small>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 text-center saradnici">
+				<img class="img-responsive" src="img/saradnici/radivoj.jpg" alt="">
+				<h3>Radivoj Paroški<br>
+					<small>media</small>
 				</h3>
 				
-				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#saradnik3">Pročitaj više</a></div>
+				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#radivoj">Pročitaj više</a></div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 text-center saradnici">
+				<img class="img-responsive" src="img/saradnici/emese.jpg" alt="">
+				<h3>Emese Backai<br>
+					<small>makeup educator</small>
+				</h3>
+				
+				<div class="text-center"><a href="" class="btn btn-default btn-lg" data-toggle="modal" data-target="#emese">Pročitaj više</a></div>
 			</div>
 			<div class="clearfix"></div>
 		</div>
@@ -73,16 +81,16 @@
 <!-- Button trigger modal -->
 
 <!-- Modal -->
-<div class="modal fade" id="saradnik1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="istvan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Saradnik3</h4>
+        <h4 class="modal-title" id="myModalLabel">Istvan Czekus</h4>
       </div>
       <div class="modal-body">
-        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        <img class="img-responsive img-centered" src="img/saradnici/istvan.jpg" alt="Istvan Czekus">
+		<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
 		aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -99,16 +107,16 @@
   </div>
 </div>
 
-<div class="modal fade" id="saradnik2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="goca" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Saradnik3</h4>
+        <h4 class="modal-title" id="myModalLabel">Goca Miladinović</h4>
       </div>
       <div class="modal-body">
-        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        <img class="img-responsive img-centered" src="img/saradnici/goca.jpg" alt="Goca Miladinović">
+		<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
 		aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -125,16 +133,42 @@
   </div>
 </div>
 
-<div class="modal fade" id="saradnik3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="radivoj" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Saradnik3</h4>
+        <h4 class="modal-title" id="myModalLabel">Radivoj Paroški</h4>
       </div>
       <div class="modal-body">
-        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        <img class="img-responsive img-centered" src="img/saradnici/radivoj.jpg" alt="Radivoj Paroški">
+		<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+		aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Zatvori</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="emese" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Emese Backai</h4>
+      </div>
+      <div class="modal-body">
+        <img class="img-responsive img-centered" src="img/saradnici/emese.jpg" alt="Emese Backai">
+		<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
 		aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
