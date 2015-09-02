1a978e8c20935798c190a2def58a72f5f9379df9
Dragan Vidakovic <vidakovic.dragan981@gmail.com>
2015-09-02 20:36:48 +0200

    nesto

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php include("config/setup.php"); ?>
-<?php include(D_TEMPLATE.'/header.php'); ?>
-	
-	
-
-		<?php include('views/'.$page['slug'].'.php');//View Type ?>
-		
-	
-	
-
-	
-	<?php include(D_TEMPLATE.'/footer.php'); ?>
+<?php include(D_TEMPLATE.'/header.php'); ?>	
+<?php include('views/'.$page['slug'].'.php');//View Type ?>
+<?php include(D_TEMPLATE.'/footer.php'); ?>
