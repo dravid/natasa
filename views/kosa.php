7f40900e4400806db41226a55d3042cb299a88c0
dravid <vidakovic.dragan981@gmail.com>
2015-09-06 23:00:53 +0200

    Ubacio tekst i sredio

--- views/kosa.php
+++ views/kosa.php
@@ -49,11 +49,12 @@
 
 				
 				<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-				non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Volim svoj posao! Ovu frizuru i šminku za venčanje od Jordan Fisher iz Washington DC, USA radila sam 
+				u Perastu. Fotografiju je radio Lexillium - Aleksandar Jaredić iz Podgorice. Od šminke sam (naravno) 
+				koristila Atelier a za kosu sam koristila Alfaparfove preparate: uvek prisutni Sculpting spray, 
+				Diamond kremu za zaglađivanje, lak za kosu za učvrsćivanje i to je sve!!! Fen, četka, kupa, šestice, 
+				pletenica u koju smo zadenuli cvetić i maslinovu grančicu iz buketa. Ona je osmislila frizuru a ja je 
+				sprovela u delo. Za sve informacije o proizvodima slobodno me kontaktirajte.
 				</p>
 				
 			</div>
