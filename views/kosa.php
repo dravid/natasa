4abf5e2008884b9020a26becf38285f56c592679
Dragan Vidakovic <vidakovic.dragan981@gmail.com>
2015-09-06 20:45:45 +0200

    Kosa i sminka i popravke

---
+++ views/kosa.php
@@ -0,0 +1,74 @@
+<style type="text/css"> @import url("css/kosa.css"); </style>
+<div class="container">
+
+	<div class="row">
+		<div class="box">
+		
+			<div class="col-lg-12">
+				<hr>
+				<h2 class="intro-text text-center">Stranica posvećena 
+					<strong>kosi</strong>
+				</h2>
+				<hr>
+			
+			
+				<div class="video_box" id="promo">
+					<img src="img/kosa/kosa_header.jpg" class="img-responsive img-centered" alt="Šminkanje">
+				</div>
+				
+			</div>
+		</div><!-- /.box -->
+	</div><!-- /.row -->
+	
+	<div class="row">
+		<div class="box">
+		
+			<div class="col-lg-12">
+				<hr>
+					<h2 class="intro-text text-center">American wedding in Montenegro</h2>
+				<hr>
+			</div>
+		
+			<div class="col-lg-7" id="american_wedding_left_img">
+				
+				<a href="img/kosa/american_wedding1_full.jpg" rel="prettyPhoto[american_wedding]" title="American wedding1" alt="American wedding1" >  
+					<img src="img/kosa/american_wedding1.jpg" class="img-responsive pull-left" title="American wedding1" alt="American_wedding1">
+				</a>
+				
+				<a href="img/kosa/american_wedding2_full.jpg" rel="prettyPhoto[american_wedding]" title="American wedding2" alt="American wedding2" >  
+					<img src="img/kosa/american_wedding2.jpg" class="img-responsive pull-left" title="American wedding2" alt="American wedding2">
+				</a>
+				
+				<a href="img/kosa/american_wedding3_full.jpg" rel="prettyPhoto[american_wedding]" title="American wedding3" alt="American wedding3" >  
+					<img src="img/kosa/american_wedding3.jpg" class="img-responsive pull-left" title="American wedding3" alt="American wedding3">
+				</a>
+				
+				<a href="img/kosa/american_wedding4_full.jpg" rel="prettyPhoto[american_wedding]" title="American wedding4" alt="American wedding4" >  
+					<img src="img/kosa/american_wedding4.jpg" class="img-responsive pull-left" title="American wedding4" alt="American wedding4">
+				</a>
+
+				
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+				non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				
+			</div>
+			
+			<div class="col-lg-5">
+				
+				<a href="img/kosa/american_wedding5_full.jpg" rel="prettyPhoto[american_wedding]" title="American wedding5" alt="American wedding5" >  
+					<img src="img/kosa/american_wedding5.jpg" class="img-responsive img-centered" title="American wedding5" alt="American wedding5">
+				</a>
+			
+				
+			</div>
+
+	<div class="clearfix"></div>
+		</div><!-- /.box -->
+	</div><!-- /.row -->
+	
+</div><!-- /.container -->
---
+++ views/sminka.php
@@ -0,0 +1,23 @@
+<style type="text/css"> @import url("css/sminka.css"); </style>
+<div class="container">
+
+	<div class="row">
+		<div class="box">
+		
+			<div class="col-lg-12">
+				<hr>
+				<h2 class="intro-text text-center">Stranica posvećena 
+					<strong>šminki</strong>
+				</h2>
+				<hr>
+			
+			
+				<div class="video_box" id="promo">
+					<img src="img/sminka/sminka_header1.jpg" class="img-responsive img-centered" alt="Šminkanje">
+				</div>
+				
+			</div>
+		</div><!-- /.box -->
+	</div><!-- /.row -->
+	
+</div><!-- /.container -->
